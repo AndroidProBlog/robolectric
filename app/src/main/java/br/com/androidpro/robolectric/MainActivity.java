1707e82207856768d858ec0025d23e7d36146c1f
Thiago Pagonha <thi.pag@gmail.com>
2015-09-02 19:52:20 -0300

    Initial Import

---
+++ app/src/main/java/br/com/androidpro/robolectric/MainActivity.java
@@ -0,0 +1,29 @@
+package br.com.androidpro.robolectric;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    int numberOfClicks = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberOfClicks++;
+                button.setText("Clicked " + numberOfClicks + " times!");
+            }
+        });
+    }
+}
